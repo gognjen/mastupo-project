f7390f833e2af110b86ec8a217c6c5242d5f0aee
Ognjen Glišić <ognjen@inbox.com>
2014-07-27 04:09:16 +0200

    fixed wsgi file

--- mastupo/wsgi.py
+++ mastupo/wsgi.py
@@ -7,12 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 """
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mastupo.settings")
+
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 """
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mastupo.settings")
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
